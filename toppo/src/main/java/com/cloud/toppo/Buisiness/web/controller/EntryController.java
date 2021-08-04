f3394200fa03ef0833006ae69bce68c1818bbf3f
末平滉明 <suehirahiroshiakira@suehirahiroshimeinoMacBook-Air.local>
2021-08-04 22:31:33 +0900

    ぽぽぽl

---
+++ toppo/src/main/java/com/cloud/toppo/Buisiness/web/controller/EntryController.java
@@ -0,0 +1,11 @@
+package com.cloud.toppo.Buisiness.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+//@RequestMapping(path = "/")
+@SessionAttributes("entry")
+public class EntryController {
+
+}
