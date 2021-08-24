5d8ee307c1fa5168773e68cfc574a2cef7d447e2
iidasuzuka <szkiid.622@gmail.com>
2021-08-24 22:46:52 +0900

    userDao,Service

--- toppo/src/main/java/com/cloud/toppo/Buisiness/Dao/userDao.java
+++ toppo/src/main/java/com/cloud/toppo/Buisiness/Dao/userDao.java
@@ -1,12 +1,15 @@
 package com.cloud.toppo.Buisiness.Dao;
 
+import org.apache.ibatis.annotations.Insert;
 import org.springframework.stereotype.Repository;
+
+import com.cloud.toppo.Buisiness.Domain.UserModel;
 
 @Repository
 public interface userDao {
 
-	//hello
+	//ユーザー登録処理
+	@Insert("INSERT INTO USER (USER_ID,FRONTBACK_ID,USER_NAME,PASSWORD,MAIL,INTRODUCE,IMAGE) VALUES (#{userId},#{frontBackId},#{userName},#{password},#{mail},#{introduce},#{image});")
+	public int insertUser(UserModel UserModel);
 	
-	
-	//pppppppppp
 }
--- toppo/src/main/java/com/cloud/toppo/Buisiness/Service/UserServiceImpl.java
+++ toppo/src/main/java/com/cloud/toppo/Buisiness/Service/UserServiceImpl.java
@@ -1,9 +1,29 @@
 package com.cloud.toppo.Buisiness.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cloud.toppo.Buisiness.Dao.userDao;
+import com.cloud.toppo.Buisiness.Domain.UserModel;
+
+
 
 
 @Service
 public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	userDao userDao;
+	
+	//ユーザー登録処理
+	@Override
+	@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.READ_COMMITTED, timeout = 10, readOnly = false, rollbackFor = Exception.class)
+	public UserModel inserttUser(UserModel userModel) {
+		userDao.insertUser(userModel);
+		return userModel;
+	}
 
 }
